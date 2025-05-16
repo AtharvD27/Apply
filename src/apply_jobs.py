dc07673584ab6a9c8ffa3f227227e8cd28d06630
Atharv Dabhade <atharvd2077@gmail.com>
2025-05-16 01:39:22 -0400

    Update apply_jobs.py

--- src/apply_jobs.py
+++ src/apply_jobs.py
@@ -72,17 +72,17 @@ def login_to_dice(driver):
     logger.info("Logging into Dice...")
     print("Logging into Dice...")
     driver.get("https://www.dice.com/dashboard/login")
-    WebDriverWait(driver, DELAY).until(
+    WebDriverWait(driver, DELAY+10).until(
         EC.presence_of_element_located((By.NAME, "email"))
     ).send_keys(EMAIL)
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-    WebDriverWait(driver, DELAY).until(
+    WebDriverWait(driver, DELAY+10).until(
         EC.presence_of_element_located((By.NAME, "password"))
     ).send_keys(PASSWORD)
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-    WebDriverWait(driver, DELAY).until(EC.url_contains("dashboard"))
+    WebDriverWait(driver, DELAY+10).until(EC.url_contains("dashboard"))
     logger.info("Login successful.")
     print("Login successful.")
 
